2d24ac9bf4a70ee3d15b859ed762abe775db43be
joshbarker <abmjosh@gmail.com>
2015-03-24 18:38:47 +0900

    Associate and neste topics and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
-Rails.application.routes.draw do
-
+Bloccit::Application.routes.draw do
 
   devise_for :users
-    resources :posts, :advertisements
- 
-    get 'about' => 'welcome#about'
+
+   resources :topics do
+     resources :posts, except: [:index]
+   end
+
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 end
