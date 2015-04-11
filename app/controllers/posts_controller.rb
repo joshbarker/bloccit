07b5774ce00a434960286762a5fd8ede1d4e71c7
joshbarker <abmjosh@gmail.com>
2015-04-11 10:17:32 +0900

    Mocking With RSPEC

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
     authorize @post
 
     if @post.save_with_initial_vote
+      @post.create_vote
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
